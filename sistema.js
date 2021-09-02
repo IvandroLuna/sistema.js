7c6babc903f9e0f4cd125e14101ade0ceef26978
Ivandro Luna <ivandroluna7@gmail.com>
2021-09-01 22:12:45 -0300

    sistema.js

---
+++ sistema.js
@@ -0,0 +1,12 @@
+let numeroDeAlunos = 10;
+
+for (let contador = 0; contador <= numeroDeAlunos; contador++) 
+   // console.log(contador);
+    
+if (contador == 0) {
+    console.log("O numero é ZERO");  
+} else if (contador % 2 == 0) {
+    console.log("O numero " + contador +" É PAR")
+} else {
+    console.log("O numero " + contador + " É impar")
+} 
